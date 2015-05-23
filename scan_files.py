6cfc2497c6eb2bd913d1ce0df28e378789427b9a
Taewon Kim <kimt33@mcmaster.ca>
2015-05-23 19:23:13 -0400

    added scan_files.py: for scanning through directories of some characteristic

---
+++ scan_files.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+cwd = os.getcwd()
+
+def farnaz_format(dirname, filename):
+    """ generates gaussian files organized by farnaz
+    
+    level0: molecule grouping
+    level1: molecule identifier
+    level2: method/basis set
+    level3: output files (gaussian.com, gaussian.log, gaussian.out, gaussian.chk)
+
+    ASSUMES:
+        you are on level0
+    Args:
+        dirname: string that is the name of the molecule grouping directory
+    """
+    for dirpath, dirnames, filenames in os.walk(dirname):
+        if len(dirpath.split(os.sep))==3 and filename in filenames:
+            yield os.path.join(dirpath, filename)
