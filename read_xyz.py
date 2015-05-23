f450a6ca5a3423738d825443d4048b354cc61d5e
Taewon Kim <kimt33@mcmaster.ca>
2015-05-23 00:18:53 -0400

    added xyz reader

--- read_xyz.py
+++ read_xyz.py
@@ -1,4 +1,5 @@
 from horton.io.smartreader import *
+import re
 
 class XYZReader:
     """reads xyz file
@@ -10,16 +11,30 @@ class XYZReader:
         Args:
             inpfile: string that describes the location of xyz file
         """
-        self.filename=inpfile
-        self.title=''
-        self.num_atoms=0
-        self.atoms=[]
-        self.coordinates=[]
-        extract_xyz()
+        self.filename = inpfile
+        # FUCK TITLES
+        #self.title=''
+        self.num_atoms = []
+        self.atoms = []
+        self.coordinates = []
+        self.extract_xyz()
 
     def extract_xyz(self):
         """ extracts the informations from self.filename
 
         """
+        re_numatom = re.compile(r'^\s*(\d+)\s*$')
+        #re_title = re.compile(r'^\s*(.*)\s*$')
+        re_coords = re.compile(r'^\s*(\w+)\s+([\-\d\w\.eEdD]+)\s+([\-\d\w\.eEdD]+)\s+([\-\d\w\.eEdD]+)\s*$')
+        item_numatom = Item(re_numatom, max_match=-1)
+        #item_title = Item(re_title, header=item_numatom, max_match=1)
+        item_coords = Item(re_coords, header=item_numatom, max_match=-1)
         with open(self.filename, 'r') as f:
-            
+            for line in f:
+                item_numatom(line)
+                item_coords(line)
+                #item_title(line)
+        for i in item_coords.value:
+            self.num_atoms.append(len(i))
+            self.atoms.append(zip(*i)[0])
+            self.coordinates.append([[float(k) for k in j] for j in zip(*zip(*i)[1:])])
