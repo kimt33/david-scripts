81bd60ed86ce6fca1908a3a947486e397572f461
Taewon D. Kim <kimt33@mcmaster.ca>
2016-06-03 10:07:57 -0400

    Add scripts to load different paths for ACML and MKL libraries

---
+++ load_acml.sh
@@ -0,0 +1,6 @@
+module load acml/gfortran-int64/5.3.0
+export LD_LIBRARY_PATH=/work/kimt33/local/chemps-acml/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/sharcnet/acml/5.3.0/gfortran-64bit-int64/gfortran64_int64/lib:${LD_LIBRARY_PATH}
+export CPATH=/work/kimt33/local/chemps-acml/include:${CPATH}
+export C_INCLUDE_PATH=/work/kimt33/local/chemps-acml/include:${C_INCLUDE_PATH}
+export CPLUS_INCLUDE_PATH=/work/kimt33/local/chemps-acml/include:${CPLUS_INCLUDE_PATH}
---
+++ load_mkl.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=/work/kimt33/local/chemps-mkl/lib64:$LD_LIBRARY_PATH
+export CPATH=/work/kimt33/local/chemps-mkl/include:${CPATH}
+export C_INCLUDE_PATH=/work/kimt33/local/chemps-mkl/include:${C_INCLUDE_PATH}
+export CPLUS_INCLUDE_PATH=/work/kimt33/local/chemps-mkl/include:${CPLUS_INCLUDE_PATH}
+export LD_PRELOAD=${MKLROOT}/lib/intel64/libmkl_rt.so
